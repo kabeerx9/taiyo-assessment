4db3363cf26f8e04c7b438b1073a7921c67764fb
kabeerx9 <kabeer786joshi@gmail.com>
2024-08-28 15:23:07 +0530

    ------COMPLETED------

--- src/_components/Layout.tsx
+++ src/_components/Layout.tsx
@@ -18,26 +18,25 @@ function Layout() {
 	const currentRoute = routes.find((route) => route.path === location.pathname);
 
 	return (
-		<div className="flex flex-col h-screen">
-			<header className="bg-white shadow-md p-4 flex justify-between items-center">
-				<div className="flex items-center">
-					<button className="text-blue-600 lg:hidden" onClick={toggleSidebar}>
-						☰
-					</button>
-					<h1 className="text-xl font-bold text-blue-600 ml-4">
-						Taiyo.AI Assessment
-					</h1>
+		<div className="flex h-screen w-screen">
+			<Sidebar isOpen={isSidebarOpen} closeSidebar={closeSidebar} />
+			<div className="flex flex-col w-full ">
+				<header className="bg-white shadow-md p-4 flex justify-between items-center border-b-2 border-indigo-200">
+					<div className="flex items-center">
+						<button className="text-blue-600 lg:hidden" onClick={toggleSidebar}>
+							☰
+						</button>
+					</div>
+					<div className="text-xl underline font-semibold text-black ">
+						{currentRoute?.name + ' Page' || 'No route '}
+					</div>
+					<div className="text-blue-600">👤</div>
+				</header>
+				<div className="flex flex-grow overflow-hidden">
+					<main className="flex-grow bg-gray-100 overflow-y-auto ">
+						<Outlet />
+					</main>
 				</div>
-				<div className="text-lg font-semibold text-blue-600 hidden md:block">
-					{currentRoute?.name || 'No route '}
-				</div>
-				<div className="text-blue-600">👤</div>
-			</header>
-			<div className="flex flex-grow overflow-hidden">
-				<Sidebar isOpen={isSidebarOpen} closeSidebar={closeSidebar} />
-				<main className="flex-grow bg-gray-100 overflow-y-auto ">
-					<Outlet />
-				</main>
 			</div>
 		</div>
 	);
