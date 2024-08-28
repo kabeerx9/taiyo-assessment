98c331a638dc6f63fc021767f60287fc0a30fe93
kabeerx9 <kabeer786joshi@gmail.com>
2024-08-28 08:35:08 +0530

    React Leaflet -> Implementation done(js)

--- src/_components/charts-and-maps.tsx
+++ src/_components/charts-and-maps.tsx
@@ -7,19 +7,18 @@ type Props = {};
 
 const ChartsAndMaps = (props: Props) => {
 	return (
-		<div className="w-full h-full  flex flex-col gap-10 px-4">
+		<div className="w-full h-full px-4">
 			<div className="text-center font-semibold text-xl">
 				Charts and Maps Page
 			</div>
 			<CovidLineGraph />
+			<Temp />
 			{/* The First api endpoint provided for all cases in the world , i have no
 			idea how to use it and what is the use as in the final deliverables it is
 			not mentioned anywhere . So i have created a component , but not rendering
 			it .
 			 */}
 			{/* <CovidStatsGraph /> */}
-
-			<Temp />
 
 			{/* <CountrySpecificLeaflet /> */}
 		</div>
--- src/_components/charts-and-maps/temp.jsx
+++ src/_components/charts-and-maps/temp.jsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useQuery } from '@tanstack/react-query';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const Temp = () => {
+// Fix for default marker icon
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+	iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+	iconUrl: require('leaflet/dist/images/marker-icon.png'),
+	shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+const COUNTRIES_DATA_URL = 'https://disease.sh/v3/covid-19/countries';
+
+const fetchCountriesData = async () => {
+	const response = await fetch(COUNTRIES_DATA_URL);
+	const data = await response.json();
+	return data;
+};
+
+const CovidMap = () => {
+	const { data: countries, isLoading } = useQuery({
+		queryFn: fetchCountriesData,
+		queryKey: ['countries'],
+		staleTime: Infinity,
+	});
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	if (!countries) {
+		return <div>No data available</div>;
+	}
+
 	return (
-		<div>
-			{' '}
-			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				<Marker position={[51.505, -0.09]}>
+		<MapContainer center={[20, 0]} zoom={2}>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			{countries?.map((country) => (
+				<Marker
+					key={country.country}
+					position={[country.countryInfo.lat, country.countryInfo.long]}>
 					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
+						<div>
+							<h3>{country.country}</h3>
+							<p>Active cases: {country.active}</p>
+							<p>Recovered: {country.recovered}</p>
+							<p>Deaths: {country.deaths}</p>
+						</div>
 					</Popup>
 				</Marker>
-			</MapContainer>
-		</div>
+			))}
+		</MapContainer>
 	);
 };
 
-export default Temp;
+export default CovidMap;
